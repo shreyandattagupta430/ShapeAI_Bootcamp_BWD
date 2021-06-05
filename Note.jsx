10f96a6330bcd40ac54a80309bde26803605d4ee
SHREYAN DATTA GUPTA <67722966+shreyandattagupta430@users.noreply.github.com>
2021-06-05 13:33:54 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        We covered bootcamp on Javascript and NodeJs in this bootcamp by Shaurya
+        sir
+      </p>
+    </div>
+  );
+}
+
+export default Note;
